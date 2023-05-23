6e692e2f257c4466590f3850e7ac0a9e6738ade7
JUAN DAVID OROPEZA MORA <juan.oropezam@elektra.com.mx>
2023-05-23 00:29:49 -0600

    PruebasGIT

---
+++ SIAG-back/src/interfaces/seguimientos.interface.ts
@@ -0,0 +1,12 @@
+export interface SeguimientosSet {
+  id: string;
+  idRastreo: string;
+  idFolio: string;
+  nombre: string;
+  puesto: string;
+  area: string;
+  tel: string;
+  email: string;
+  asunto: string;
+  filename: string[];
+}
