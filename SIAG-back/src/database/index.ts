14648981e868e854933ff9e451453085ce6e3431
OropezaD <davidoro95@gmail.com>
2023-05-23 00:34:38 -0600

    PruebasGIT

--- SIAG-back/src/database/index.ts
+++
@@ -1,9 +0,0 @@
-import { DB_HOST, DB_PORT, DB_DATABASE } from '@config';
-
-export const dbConnection = {
-  url: `mongodb+srv://admin_1:5nnkq6IcwQhavV53@sif01.4tjvz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-  options: {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-};
--- SIAG-front/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  backUrl: 'http://localhost:3000/'
-};
-
